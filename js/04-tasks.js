2f6c7af41ed922813403491a5839a6216c53b058
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-12-08 22:16:52 +0200

    update

--- js/04-tasks.js
+++ js/04-tasks.js
@@ -105,3 +105,41 @@ account.withdraw(500);
 
 console.log(account.getBalance());
 console.log(account.getTransactionTotal(Transaction.WITHDRAW));
+
+/* 
+
+
+
+*/
+
+/* const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  {
+    title: "The Dreams in the Witch House",
+    author: "Howard Lovecraft",
+    rating: 8.67,
+  },
+];
+const MIN_BOOK_RATING = 8;
+
+const names = books
+  .filter((book) => book.rating >= MIN_BOOK_RATING)
+  .toSorted((first, second) => first.author.localeCompare(second.author))
+  .map((book) => book.author);
+
+console.log(names); */

16609286b89cd14aa1d60617c286019cc870d936
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-02-07 18:41:51 +0200

    update

--- src/js/test.js
+++
@@ -1,9 +0,0 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import img from '../images/images.jpeg';
-
-iziToast.show({
-  title: 'Hey',
-  message: 'What would you like to add?',
-  icon: 'icon-person',
-});

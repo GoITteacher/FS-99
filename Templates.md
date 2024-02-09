573571ab01146631d496824f80811e4f89b8c4c8
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-02-09 18:00:52 +0200

    update

--- Templates.md
+++ Templates.md
@@ -1,14 +1,16 @@
-```js
-<div class="hero-card card">
-  <div class="image-container">
-    <img src="${hero.images.lg}" alt="#" class="hero-image" />
-  </div>
-  <div class="hero-body">
-    <h4 class="hero-name">${hero.name}</h4>
-    <p class="hero-bio">
-      ${hero.biography.fullName} - ${hero.biography.placeOfBirth}, $
-      {hero.work.base}
-    </p>
-  </div>
-</div>
+```html
+<li class="card book-item" data-id="${id}">
+  <h4>${id} - ${title}</h4>
+  <p>${desc}</p>
+  <p>${author}</p>
+</li>
+```
+
+```html
+<li class="card user-item" data-id="${id}">
+  <img src="${url}" alt="#" class="user-avatar" />
+  <h3>${name}</h3>
+  <p>${email}</p>
+  <p>${phone}</p>
+</li>
 ```
---
+++ src/js/modules/booksAPI.js
@@ -0,0 +1,70 @@
+export class BooksApi {
+  constructor() {
+    this.BASE_URL = 'http://localhost:3000';
+    this.END_POINT = '/books';
+  }
+
+  getBooks() {
+    const url = this.BASE_URL + this.END_POINT;
+
+    return fetch(url).then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        const myError = new Error(`${res.status}`);
+        throw myError;
+      }
+    });
+  }
+
+  createBook(book) {
+    const url = this.BASE_URL + this.END_POINT;
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    };
+
+    return fetch(url, options).then(res => res.json());
+  }
+
+  updateBook({ id, ...book }) {
+    const url = `${this.BASE_URL}${this.END_POINT}/${id}`;
+
+    const options = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    };
+
+    return fetch(url, options).then(res => res.json());
+  }
+  resetBook({ id, ...book }) {
+    const url = `${this.BASE_URL}${this.END_POINT}/${id}`;
+
+    const options = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    };
+
+    return fetch(url, options).then(res => res.json());
+  }
+
+  deleteBook(id) {
+    const url = `${this.BASE_URL}${this.END_POINT}/${id}`;
+
+    const options = {
+      method: 'DELETE',
+    };
+
+    return fetch(url, options).then(res => res.json());
+  }
+}
